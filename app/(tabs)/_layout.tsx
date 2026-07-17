5924a82841d68a45c49d78643e90132156addf2f
jerry <jerryren.5279@gmail.com>
2026-07-17 11:19:19 -0400

    Redirects to /(tabs)/places when user logs in. And redirects back to "/" when user is logs out.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,20 @@
-import { Tabs } from "expo-router";
+import { AuthContext } from "@/context/auth-context";
+import { AuthState } from "@/domain/auth/authTypes";
+import { Redirect, Tabs } from "expo-router";
+import { useContext } from "react";
 
 export default function TabsLayout() {
+  const { state } = useContext<{ state: AuthState; dispatch: React.Dispatch<any> }>(AuthContext);
+
+  if (state.userState !== "authenticated") {
+    return <Redirect href="/" />;
+  }
+
   return (
     <Tabs>
-      <Tabs.Screen name="search/index" options={{ title: "Search" }} />
-      <Tabs.Screen name="places/index" options={{ title: "Places" }} />
-      <Tabs.Screen name="profile/index" options={{ title: "Profile" }} />
+      <Tabs.Screen name="search" options={{ title: "Search" }} />
+      <Tabs.Screen name="places" options={{ title: "Places" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
     </Tabs>
   );
 }
